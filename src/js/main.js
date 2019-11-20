bcc015cf43b2cfc28d23a96120da43387d17b3f2
Will Storey <will@willstorey.ca>
2019-11-20 10:47:42 -0500

    Made style asjustments to lightbox

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,15 @@
 'use strict';
 
+const htmlEl = document.querySelector('html');
+const iframeEl = document.querySelector('iframe');
+
 document.querySelector('img').addEventListener('click', function() {
     this.classList.toggle('lightbox');
-    if(document.querySelector('html').style.background == "") {
-        document.querySelector('html').style.background = "rgb(30, 30, 30)";
+    if(htmlEl.style.background == "") {
+        htmlEl.style.background = "rgb(10, 10, 10)";
+        iframeEl.style.display = "none";
     } else {
-        document.querySelector('html').style.background = "";
+        htmlEl.style.background = "";
+        iframeEl.style.display = "initial";
     }
 });

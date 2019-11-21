00f3390a8eb066af8dfe2a1585f05d6309e55f8b
Will Storey <will@willstorey.ca>
2019-11-20 21:36:24 -0500

    Lightbox will now hide conflicting elements - Done with a background change or display none

--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,84 @@
 'use strict';
 
 const htmlEl = document.querySelector('html');
-const iframeEl = document.querySelector('iframe');
+const h2El = document.querySelectorAll('h2');
+const h3El = document.querySelectorAll('h3');
 const olEl = document.querySelector('ol');
+const thEl = document.querySelectorAll('th');
+const trEl = document.querySelectorAll('tr');
+const iframeEl = document.querySelector('iframe');
+
+const background = "rgb(5, 5, 5)";
 
 document.querySelector('img').addEventListener('click', function() {
     this.classList.toggle('lightbox');
     if(htmlEl.style.background == "") {
         this.style.width = "50%";
-        htmlEl.style.background = "rgb(10, 10, 10)";
-        document.querySelectorAll('h2').style.background = "rgb(10, 10, 10)";
-        iframeEl.style.display = "none";
+        document.querySelector('html').style.background = background;
+
+        // document.querySelector('html').style.width = "100%";
+        // document.querySelectorAll('h2').style.background = "rgb(10, 10, 10)";
+
+        if(iframeEl !== null) {
+            iframeEl.classList.toggle('hidden');
+        }
+
+        if(h2El !== undefined) {
+            h2El.forEach(el => {
+               el.style.background = background;
+            });
+        }
+
+        if(h3El !== undefined) {
+            h3El.forEach(el => {
+                el.style.background = background;
+             });
+        }
+
+        if(trEl !== undefined) {
+            trEl.forEach(el => {
+                el.style.background = background;
+             });
+        }
+
+        if(thEl !== undefined) {
+            thEl.forEach(el => {
+                el.style.background = background;
+                el.style.color = "black";
+             });
+        }
+
+        if(olEl !== null) {
+            olEl.style.background = background;
+        }
+
     } else {
         this.style.width = "";
-        htmlEl.style.background = "";
-        iframeEl.style.display = "initial";
+        document.querySelector('html').style.background = "";
+
+        if(iframeEl !== null) {
+            iframeEl.classList.toggle('hidden');
+        }
+
+        h2El.forEach(el => {
+            el.style.background = "";
+         });
+
+         h3El.forEach(el => {
+            el.style.background = "";
+         });
+
+         trEl.forEach(el => {
+            el.style.background = "";
+         });
+
+        thEl.forEach(el => {
+            el.style.background = "";
+            el.style.color = "white";
+            });
+
+        if(olEl !== null) {
+            olEl.style.background = "";
+        }
     }
 });
